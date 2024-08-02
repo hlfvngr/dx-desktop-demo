0539dcaee21234e3f8de5a4f08b68b7d1189439b
hlfvngr <870012250@qq.com>
2024-08-02 11:11:48 +0800

    切换成web应用

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,12 @@ fn main() {
     // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
     info!("starting app");
-
-    let cfg = dioxus::desktop::Config::new()
-        //.with_custom_head(r#"<link rel="stylesheet" href="tailwind.css">"#.to_string())
-        .with_custom_head(r#"<script src="bootstrap.bundle.min.js"></script>"#.to_string())
-        .with_custom_head(r#"<link href="bootstrap.min.css" rel="stylesheet">"#.to_string());
-    LaunchBuilder::desktop().with_cfg(cfg).launch(App);
+    launch(App);
+    // let cfg = dioxus::desktop::Config::new()
+    //     //.with_custom_head(r#"<link rel="stylesheet" href="tailwind.css">"#.to_string())
+    //     .with_custom_head(r#"<script src="bootstrap.bundle.min.js"></script>"#.to_string())
+    //     .with_custom_head(r#"<link href="bootstrap.min.css" rel="stylesheet">"#.to_string());
+    // LaunchBuilder::desktop().with_cfg(cfg).launch(App);
 }
 
 #[component]
